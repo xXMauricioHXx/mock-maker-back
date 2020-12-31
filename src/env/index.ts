a3ced6101d2bd1066dcc306992479fe635b296e8
Mauricio Henrique <mauricio.henrique@somosphi.com>
2020-12-31 20:21:37 -0300

    fix

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,15 +1,14 @@
-import * as dotenv from 'dotenv';
-import { EnvValidator } from './validator';
+import * as dotenv from "dotenv";
+import { EnvValidator } from "./validator";
 
-const knexfile = require('../../knexfile.js');
+const knexfile = require("../../knexfile.js");
 
 dotenv.config();
 
 const props = {
   knexConfig: knexfile,
-  httpPort:
-    (process.env.HTTP_PORT && parseInt(process.env.HTTP_PORT, 10)) || 3000,
-  httpBodyLimit: process.env.HTTP_BODY_LIMIT || '10kb',
+  httpPort: (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000,
+  httpBodyLimit: process.env.HTTP_BODY_LIMIT || "10kb",
 };
 
 export const env = new EnvValidator(props);
