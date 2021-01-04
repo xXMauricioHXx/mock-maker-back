f7d1c1c341ead048ec1058d8faa7ade275da9d76
Mauricio Henrique <mauricio.henrique@somosphi.com>
2021-01-04 15:16:41 -0300

    project list route added

--- src/container/services/project.ts
+++ src/container/services/project.ts
@@ -1,7 +1,7 @@
-import { provide, inject } from 'injection';
-import { ProjectRepository } from '../repositories/project';
-import { CreateDTO } from '../../http/dto/project';
-import { Project } from '../../types';
+import { provide, inject } from "injection";
+import { ProjectRepository } from "../repositories/project";
+import { CreateDTO } from "../../http/dto/project";
+import { Project } from "../../types";
 
 @provide()
 export class ProjectService {
@@ -12,5 +12,9 @@ export class ProjectService {
   async create(data: CreateDTO): Promise<Project | null> {
     const projectId = await this.projectRepository.create(data);
     return await this.projectRepository.getById(projectId);
+  }
+
+  async listAll(): Promise<Project[]> {
+    return this.projectRepository.all();
   }
 }
--- src/http/controllers/project.ts
+++ src/http/controllers/project.ts
@@ -1,13 +1,13 @@
-import { BaseController } from './controller';
-import { AppContainer } from '../../container';
-import { ProjectService } from '../../container/services/project';
-import { Controller, Post } from '../decorators';
-import { Request, Response, NextFunction } from 'express';
-import { CreateDTO } from '../dto/project';
-import { validatorMiddleware } from '../middlewares/validator';
-import { createSchema } from '../schemas/project';
+import { BaseController } from "./controller";
+import { AppContainer } from "../../container";
+import { ProjectService } from "../../container/services/project";
+import { Controller, Post, Get } from "../decorators";
+import { Request, Response, NextFunction } from "express";
+import { CreateDTO } from "../dto/project";
+import { validatorMiddleware } from "../middlewares/validator";
+import { createSchema } from "../schemas/project";
 
-@Controller('/project')
+@Controller("/project")
 export class ProjectController extends BaseController {
   protected projectService: ProjectService;
   constructor(container: AppContainer) {
@@ -15,12 +15,22 @@ export class ProjectController extends BaseController {
     this.projectService = container.get<ProjectService>(ProjectService);
   }
 
-  @Post('/', [validatorMiddleware(createSchema)])
+  @Post("/", [validatorMiddleware(createSchema)])
   async create(req: Request, res: Response, next: NextFunction) {
     try {
       const data: CreateDTO = req.body;
       const project = await this.projectService.create(data);
       res.status(201).send(project);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  @Get("/")
+  async listAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const projects = await this.projectService.listAll();
+      res.send(projects);
     } catch (err) {
       next(err);
     }
